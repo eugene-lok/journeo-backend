ce14b3485c955ee3b5c3ebe7ea111f2fbef5b2b7
Eugene Lok <eugeneywlok@gmail.com>
2024-10-08 01:05:03 -0600

    Add places and login request to models

--- app/models.py
+++ app/models.py
@@ -5,3 +5,16 @@ class UserItinerary(BaseModel):
     destinations: list[str]
     duration: int
     budget: int
+
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+class Place(BaseModel):
+    id: int
+    name: str
+    address: str
+    coordinates: list[Coordinates] 
+    
+class LoginRequest(BaseModel):
+    email: str
+    password: str
